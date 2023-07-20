118fe2e1ac8d0c2a4cf1ca1721a4dad03f4dff50
Andrew Ruslander <andrew.ruslander@gmail.com>
2023-07-20 08:38:45 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `The Doom Range`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
